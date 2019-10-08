55ec2a3f6352e48256d35c110aa9597c1def463b
Max Audron <audron@cocaine.farm>
2019-10-08 11:43:34 +0200

    a lot

--- bash2version.sh
+++ bash2version.sh
@@ -1,11 +1,41 @@
 #!/usr/bin/env bash
+# Sets the string to use a replace target
 VER='VERSION'
+
+# String used to signal a prerelease
 PRERELEASE_STRING='rc'
 
-# FILES,REGEX
+# Provide a list of files to modify and the regex to use for them
+# in the form of FILE;REGEX
+# use $VER
 FILES=(
-  "testfile,test $VER"
+  "testfile;test $VER"
 )
+
+# Regex used to parse semver
+# \1 = MAJOR
+# \2 = MINOR
+# \3 = PATCH
+# \5 = PRERE
+# \8 = BUILD
+SEMVER='(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.0|\.[1-9]\d*|\.\d*[a-zA-Z-][0-9a-zA-Z-]*)*)?(\+([0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*))?'
+
+help() {
+  echo "bash2version - bump your version, in style, and with violence
+
+bash2version [--set VERSION | --build BUILD | --bump major|minor|patch|prerelease]
+
+OPTIONS:
+  -b, --bump major|minor|patch|prerelease:
+      bumps the specified version segment, prerelease either adds 
+      or removes the prerelease segment ('-rc' by default)
+
+  --build BUILD:
+      add build metadata specified by BUILD
+
+  -s, --set VERSION:
+      set the version to the string provided by VERSION"
+}
 
 # Check if an argument was provided to the given option;
 check_arg_exist() {
@@ -30,48 +60,63 @@ while [[ "$1" != "" ]]; do
         -s | --set)
             check_arg_exist "$1" "$2"
             SETVERSION=$2
+            shift 2
+        ;;
+        --help)
+            help
+            exit
             shift
         ;;
     esac
 done
 
-if [ -z $BUMP ]; then
-  SETVERSION=""
-  perl -ne '/$ENV{"SEMVER"}/ and print "$1 $2 $3 $4 $5"' ${FILES[0]} | IFS=' ' read -ra PERLVER <<< "$IN"
-	MAJOR=${PERLVER[0]}
-	MINOR=${PERLVER[1]}
-	PATCH=${PERLVER[2]}
-	PRERE=${PERLVER[3]}
-	IFS=''
-	if [ "${BUMP,,}" == "major" ]; then
-		VERSION=$((MAJOR + 1)).$MINOR.$PATCH$(if [ ! -z $PRERE ]; then echo -$PRERE; fi)$(if [ ! -z $BUILD ]; then echo +$BUILD; fi)
-	fi
-	if [ "${BUMP,,}" == "minor" ]; then
-		VERSION=$MAJOR.$((MINOR + 1)).$PATCH$(if [ ! -z $PRERE ]; then echo -$PRERE; fi)$(if [ ! -z $BUILD ]; then echo +$BUILD; fi)
-	fi
-	if [ "${BUMP,,}" == "patch" ]; then
-		VERSION=$MAJOR.$MINOR.$((PATCH + 1))$(if [ ! -z $PRERE ]; then echo -$PRERE; fi)$(if [ ! -z $BUILD ]; then echo +$BUILD; fi)
-	else
-	if [ "${BUMP,,}" == "prerelease" ]; then
-		VERSION=$MAJOR.$MINOR.$((PATCH + 1))$(if [ ! -z $PRERE ]; then; else echo "-$PRERELEASE_STRING"; fi)$(if [ ! -z $BUILD ]; then echo +$BUILD; fi)
-	else
-		echo "No valid Version segment provided"
-		exit 1
-	fi
+get_version() {
+  MATCH=${FILES[0]#*;}
+  TMPVERSION=$(sed -nr "s/.*${MATCH/VERSION/$SEMVER}.*/\1;\2;\3;\5;\8/gp" ${FILES[0]%;*})
+  IFS=';' read -r MAJOR MINOR PATCH PRERE BUILDT <<< "$TMPVERSION"; IFS=''
+  echo $MAJOR $MINOR $PATCH $PRERE $BUILDT
+}
+
+set_version() {
+  if [ ! -z $SETVERSION ]; then
+    VERSION=$SETVERSION$(if [ ! -z $BUILD ]; then echo "+$BUILD"; fi)
+  elif [ ! -z $BUMP ]; then
+    if [ "${BUMP,,}" == "major" ]; then
+      VERSION=$((MAJOR + 1)).0.0$(if [ ! -z $PRERE ]; then echo -$PRERE; fi)$(if [ ! -z $BUILD ]; then echo +$BUILD; fi)
+    elif [ "${BUMP,,}" == "minor" ]; then
+      VERSION=$MAJOR.$((MINOR + 1)).0$(if [ ! -z $PRERE ]; then echo -$PRERE; fi)$(if [ ! -z $BUILD ]; then echo +$BUILD; fi)
+    elif [ "${BUMP,,}" == "patch" ]; then
+      VERSION=$MAJOR.$MINOR.$((PATCH + 1))$(if [ ! -z $PRERE ]; then echo -$PRERE; fi)$(if [ ! -z $BUILD ]; then echo +$BUILD; fi)
+    elif [ "${BUMP,,}" == "prerelease" ]; then
+      VERSION=$MAJOR.$MINOR.$PATCH$(if [ -z $PRERE ]; then echo "-$PRERELEASE_STRING"; fi)$(if [ ! -z $BUILD ]; then echo "+$BUILD"; fi)
+    else
+      echo "No valid Version segment provided"
+      exit 1
+    fi
+  fi
+}
+
+apply_version() {
+  OLDIFS=$IFS
+  IFS=';'
+
+  echo "Applying Version: $VERSION"
+
+  for i in "${FILES[@]}"; do
+    set -- $i
+    sed -ri "s/${2/VERSION/$SEMVER}/${2/VERSION/$VERSION}/g" $1
+  done
+
+  IFS=''
+}
+
+
+if [[ -z $SETVERSION && -z $BUMP ]]; then
+  help
+  exit
+else
+  get_version
+  set_version
+  apply_version
 fi
-
-
-SEMVER='(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?'
-
-OLDIFS=$IFS
-IFS=','
-
-echo $VERSION
-
-for i in "${FILES[@]}"; do
-  set -- $i
-  echo "s/${2/VERSION/$SEMVER}/${2/VERSION/$VERSION}/g"
-  perl -i -p0e "s/${2/VERSION/$SEMVER}/${2/VERSION/$VERSION}/g" $1
-done
-
 
